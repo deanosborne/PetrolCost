439f70252261acc6c6ae6223444fb3cc8ba0c29f
Dean <imdean@hotmail.co.nz>
2019-07-11 20:18:11 +1200

    0.4
    Models added

--- DjangoProject/petrolcost/home/admin.py
+++ DjangoProject/petrolcost/home/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Partner
+from .models import Partner, Deal, Consumer
+
 # Register your models here.
+
 admin.site.register(Partner)
+admin.site.register(Deal)
+admin.site.register(Consumer)
--- DjangoProject/petrolcost/home/models.py
+++ DjangoProject/petrolcost/home/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 class Partner(models.Model):
 
     PROVIDER_CHOICES =(
-        ('BP', 'Mobil'),
-        ('NPD', 'Challenge'),
-        ('Caltex', 'Z')
+        ('','BP'), ('','Mobil'),
+        ('','NPD'), ('','Challenge'),
+        ('', 'Caltex'), ('','Z')
     )
 
     user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=True)
@@ -18,3 +18,26 @@ class Partner(models.Model):
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name + ' - ' + self.provider
+
+class Deal(models.Model):
+
+    partner = models.ForeignKey('Partner', on_delete=models.SET_NULL, null=True, blank=True)
+    consumer = models.ForeignKey('Consumer', on_delete=models.SET_NULL, null=True, blank=True)
+    deal_name = models.CharField(max_length=50)
+    deal_url = models.CharField(max_length=50)
+    deal_descrip = models.TextField()
+
+    def __str__(self):
+        return self.deal_name
+
+class Consumer(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    email_address = models.EmailField(max_length=30)
+    phone_number = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
